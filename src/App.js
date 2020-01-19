45a4e15924b9eaa00ae1e7fe559b23b310939e9a
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-01-19 14:07:24 +0200

    Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -22,14 +22,6 @@ function App() {
           Wolt Restaurant List
         </p>
         <ul>{restaurantNames}</ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
